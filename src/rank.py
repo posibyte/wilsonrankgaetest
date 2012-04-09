b9738cf7a2f51811cca86ef8ce074506f6b4c05d
Matt Adams <matt@posibyte.com>
2012-04-09 00:52:18 -0500

    Added useless time calculation

--- src/rank.py
+++ src/rank.py
@@ -1,12 +1,4 @@
-from datetime import datetime, timedelta
 from math import log, sqrt
-
-epoch = datetime(1970, 1, 1)
-
-def epoch_seconds(date):
-    """ Return number of seconds from the epoch """
-    today = date - epoch
-    return today.days * 86400 + today.seconds + (float(today.microseconds) / 1000000)
 
 def rank(up, down, confidence=1.6):
     n = up + down
